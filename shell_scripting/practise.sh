d3f64d3920e85eb16c91b8bf8380f3aec742654c
hashir <hashirsmartyy@gmail.com>
2025-01-07 21:19:07 +0500

    Refactor shell script to include input validation and directory check; add output to test.txt

--- shell_scripting/practise.sh
+++ shell_scripting/practise.sh
@@ -16,14 +16,34 @@ while [ $count -le 5 ]; do
 done
 
 # get input
-name=""
-echo "enter name pls"
-read name
-echo "your name is $name"
+# name=""
+# echo "enter name pls"
+# # read name
+# echo "your name is $name"
 
-# if else
-if [ "$name" = "hashir" ]; then
-    echo "names match!"
+# # if else
+# if [ "$name" = "hashir" ]; then
+#     echo "names match!"
+# else
+#     echo "names do not match"
+# fi
+
+test_name=""
+if [ -z "$test_name" ]; then
+    echo "test name is empty"
 else
-    echo "names do not match"
+    echo "test name is not empty"
 fi
+
+
+directory="../binary_semaphore"
+if [ -d "$directory" ]; then
+    echo "directory exists"
+else
+    echo "directory doesnt exist"
+fi
+
+echo "hashir is cool" >> test.txt | cat test.txt   #>> appends to file
+
+name=""
+read name | echo "name is $name"
